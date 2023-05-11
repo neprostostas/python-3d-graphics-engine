f2772f7589e6e427fb783bd4e6a1eb5744ad7027
neprostostas <neprostostas.gg@gmail.com>
2023-05-12 02:16:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# 🎮🕹️💡 Computer Graphics 💡🕹️🎮
+## RGR | Modeling moving objects on stage
+A simple graphics engine with Pygame and ModernGL to learn 3D programming without the use of a full game engine.
+
+## 👻 Author
+
+Created by **Stanislav Kinash** / **neprostostas**
+
+![preview1](/screenshot/1.png)
+![preview2](/screenshot/2.png)
+
+## Features
+* OBJ file importer
+* Multiple textures support
+* Simple lighting system (ambient, diffuse, specular)
+* Shadow mapping
+* Skybox
+* Simple Camera system
+* Basic Meshes Shapes
+
+## Controls
+* Movement: `WASD` keys
+* Up/Down: `Q`, `E` keys
+* Look around: Mouse
+* Quit: `ESC` key
+
+## Usage
+* Clone the project to your local machine.
+* Open terminal and navigate to the project directory.
+* Install dependencies using `pip install -r requirements.txt`.
+* Run the application using `python main.py`.
---
+++ scene.py
@@ -0,0 +1,38 @@
+from model import *
+
+
+class Scene:
+    def __init__(self, app):
+        self.app = app
+        self.objects = []
+        self.load()
+        # skybox
+        self.skybox = AdvancedSkyBox(app)
+
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+    def load(self):
+        app = self.app
+        add = self.add_object
+
+        # floor
+        n, s = 20, 2
+        for x in range(-n, n, s):
+            for z in range(-n, n, s):
+                add(Cube(app, pos=(x, -s, z)))
+
+        # columns
+        for i in range(9):
+            add(Cube(app, pos=(15, i * s, -9 + i), tex_id=2))
+            add(Cube(app, pos=(15, i * s, 5 - i), tex_id=2))
+
+        # cat
+        add(Cat(app, pos=(0, -1, -10)))
+
+        # moving cube
+        self.moving_cube = MovingCube(app, pos=(0, 6, 8), scale=(3, 3, 3), tex_id=1)
+        add(self.moving_cube)
+
+    def update(self):
+        self.moving_cube.rot.xyz = self.app.time
